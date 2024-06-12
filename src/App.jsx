ee3b36ff5db2facee0a95cb083c742417bd8283c
adomasr <adomas.razmus@gmail.com>
2024-06-12 03:35:28 +0100

    upload file functionality and sending to the server

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from 'axios';
+
 import './App.css';
 
 
 export default function App() {
-    const testUrl = 'http://localhost:5000/test';
-    const click = ()=> {
-        fetch(testUrl) 
-            .then((response)=> response.json())
-            .then((data)=> console.log(data.message));
+    const [text, setText] = useState(null);
+
+    const sendText = async ()=> {
+        const testURL = 'http://localhost:5000/test';           /*change the url */
+
+        const data = {
+            "text": text
+        };
+
+        const response = await axios.post(testURL, data);  /*set up try and catch*/
+        // console.log(response.data.message);
+        console.log(text);
+    };
+
+    const saveFile = (event)=> {
+        const inputFile = event.target.files[0];
+        const fileReader = new FileReader();
+
+        fileReader.onload = (e)=> {
+            setText(e.target.result);
+        };
+
+        try {
+            fileReader.readAsText(inputFile);
+
+        } catch(err) {
+            console.log(`The problem: ${err}`);
+        };
     };
 
     return(
         <>
             <h1>working!</h1>
-            <button onClick={click}>Click Me</button>
+            <input type='file' onChange={saveFile} />
+            <button onClick={sendText}>Get it!</button>
         </>
     );
 };
